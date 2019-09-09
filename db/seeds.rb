67c4aa6fc9b300ab30080e5eafd062993f72ab9e
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-09-09 15:02:10 -0400

    seeded database and rendered to index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create(name: "The Who", img: "https://townsquare.media/site/295/files/2014/10/TheWho.jpg?w=980&q=75")
+Artist.create(name: "KISS", img: "https://www.rollingstone.com/wp-content/uploads/2018/06/rs-14909-kiss-1800-1396461735.jpg")
+Artist.create(name: "Alice Cooper", img: "https://www.biography.com/.image/t_share/MTE5NDg0MDU1MzgyNDkyNjg3/portrait-of-alice-cooper-in--makeup.jpg")
+Artist.create(name: "Guns n Roses", img: "https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg")
+Artist.create(name: "The Rolling Stones", img: "https://yt3.ggpht.com/a/AGF-l7-n03HoZcEKQJ0raP5rKQvY02N5-Bpfg6K_YQ=s900-c-k-c0xffffffff-no-rj-mo")
