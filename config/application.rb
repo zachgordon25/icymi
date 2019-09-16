eb2e01fdf6eff70d524352d4ff45dd6545a30846
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-09-16 18:52:51 -0400

    attempted semantic ui

--- config/application.rb
+++ config/application.rb
@@ -11,24 +11,6 @@ module IcymiApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    
-    # config.before_configuration do
-    #   env_file = File.join(Rails.root, 'config', 'database.yml')
-    #   YAML.load(File.open(env_file)).each do |key, value|
-    #     ENV[key.to_s] = value
-    #   end if File.exists?(env_file)
-    # end
-
-    # config_files = ['database.yml']
-    # config_files.each do |file_name|
-    #   file_path = File.join(Rails.root, 'config', file_name)
-    #   config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
-    #   config_keys.each do |key, value|
-    #     ENV[key.upcase] ||= value
-    #   end
-    # end
-    
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
