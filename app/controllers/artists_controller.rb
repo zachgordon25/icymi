c3adc9c7366f0d3975e57229b0d06b1aaa1befd9
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-09-16 14:27:18 -0400

    cleaned up code

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,6 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show]
 
-
   # GET /artists
   # GET /artists.json
   def index
@@ -10,58 +9,9 @@ class ArtistsController < ApplicationController
 
   # GET /artists/1
   # GET /artists/1.json
-  def show
-    @artist = Artist.find(params[:id])
-   render json: {artist: @artist}
-  end
-
-  # GET /artists/new
-  # def new
-  #   @artist = Artist.new
-  # end
-
-  # GET /artists/1/edit
-  # def edit
-  # end
-
-  # POST /artists
-  # POST /artists.json
-  def create
-    @artist = Artist.new(artist_params)
-    
-    respond_to do |format|
-      if @artist.save
-        format.html { redirect_to @artist, notice: 'Artist was successfully created.' }
-        format.json { render :show, status: :created, location: @artist }
-      else
-        format.html { render :new }
-        format.json { render json: @artist.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # # PATCH/PUT /artists/1
-  # # PATCH/PUT /artists/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @artist.update(artist_params)
-  #       format.html { redirect_to @artist, notice: 'Artist was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @artist }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @artist.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # DELETE /artists/1
-  # # DELETE /artists/1.json
-  # def destroy
-  #   @artist.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to artists_url, notice: 'Artist was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
+  # def show
+  #   @artist = Artist.find(params[:id])
+  #  render json: {artist: @artist}
   # end
 
   private
@@ -70,8 +20,4 @@ class ArtistsController < ApplicationController
       @artist = Artist.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def artist_params
-      params.require(:artist).permit(:name, :img)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists do
-    resources :shows do
+  resources :artists, only: [:index] do 
+    resources :shows, only: [:index, :show] do
         end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
