1e7544d6e25d5f08855d66bb39aed94fd3289043
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-09-16 15:06:31 -0400

    deployed

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,7 +5,6 @@ class ArtistsController < ApplicationController
   # GET /artists.json
   def index
     @artists = Artist.all
-    # render json: {status: 200, message: 'linked'}
   end
 
   # GET /artists/1
